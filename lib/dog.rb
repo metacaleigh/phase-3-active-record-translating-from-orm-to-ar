ca7ad686decc23de09bedc6d55d8ca714d4cbf62
caleigh <caleighsteill@outlook.com>
2023-02-04 12:31:51 -0700

    commit tree 2ef0bea700aa5d1064ab5c877f32ae3d811be44e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,16 @@
-class Dog
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/dogs.sqlite"
+)
+
+sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+    )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+class Dog < ActiveRecord::Base
 end
